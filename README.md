560c951050afb23bd413b04673189f1169f8f44b
Iván Alfredo López Barrera <ivan23.barrera@gmail.com>
2025-06-21 16:42:05 -0600

    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Connect4
+Connect4 is the classic "connect four" game made with HTML, CSS, and JS, available in Spanish and English. Play alone or with friends and unlock achievements on the site.
---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Connect4</title>
+    <meta name="description" content="This is classic connect four game made with HTML, CSS, and JS, available in Spanish and English. Play alone or with friends and unlock achievements on the site.">
+    <meta name="keywords" content="connect 4, connect four, game, online game, connect 4 online, connect 4 offline, connect four online, connect four offline">
+</head>
+
+<body>
+    <h1>Connect 4</h1>
+</body>
+
+</html>
