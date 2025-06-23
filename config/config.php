2d437fca34bd9fffd5e852e62428a2423b7819bf
Iván Alfredo López Barrera <ivan23.barrera@gmail.com>
2025-06-22 19:49:22 -0600

    Chore: This change was made for the previous commit

--- config/config.php
+++ config/config.php
@@ -9,7 +9,9 @@ define('BASE', str_replace('\\', '/', rtrim(__DIR__, 'config')));
 define('HTML_BASE', 'http://connect4.test/');
 
 //-Database
+define('DB_MANAGER', 'mysql');
 define('DB_HOST', '127.0.0.1');
+define('DB_PORT', '3306');
 define('DB_DATABASE', 'connect4');
 define('DB_USER', 'root');
 define('DB_PASSWORD', '');
