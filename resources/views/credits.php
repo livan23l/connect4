a40ef6aacdd94b7a49573fecbfb3ef927a987814
Iván Alfredo López Barrera <ivan23.barrera@gmail.com>
2025-07-06 18:24:56 -0600

    Chore: Adding the sound to the credits

--- resources/views/credits.php
+++ resources/views/credits.php
@@ -5,3 +5,5 @@
 <a href="https://www.flaticon.es/iconos-gratis/francia" title="francia iconos">Francia iconos creados por Freepik - Flaticon</a>
 
 <a href="https://www.flaticon.es/iconos-gratis/brasil" title="brasil iconos">Brasil iconos creados por iconset.co - Flaticon</a>
+
+<a href="https://freesound.org/people/Speedenza/sounds/168039/">Whoosh woow a58cv3.wav</a> by <a href="https://freesound.org/people/Speedenza/">Speedenza</a> modified by me. License: <a href="https://creativecommons.org/licenses/by-nc/4.0/">Attribution NonCommercial 4.0</a>
