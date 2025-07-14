a146c82a890b4c5abe9dd9ca2612e934d722966a
Iván Alfredo López Barrera <ivan23.barrera@gmail.com>
2025-07-14 13:32:31 -0600

    Feat: Add the message in tooltip form and correct the styles for responsive

--- resources/views/access.php
+++ resources/views/access.php
@@ -12,11 +12,16 @@ require_once BASE . 'resources/components/header.php';
 <form id="form-signin" class="form form--hidden form--red" action="/signin" method="post" novalidate>
     <div class="form__content">
         <h2 class="form__title" data-translate="Login">Login</h2>
-        <!-- <p class="form__description" data-translate="Connect4 wants to be a fast-paced entertainment site. We'll never ask for your email address to enjoy our content. Sign in or create an account with just your username and password.">
-            Connect4 wants to be a fast-paced entertainment site. We'll never
-            ask for your email address to enjoy our content. Sign in or create
-            an account with just your username and password.
-        </p> -->
+        <div class="form__tooltip">
+            <svg class="icon icon--xs" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16">
+                <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16m.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2"/>
+            </svg>
+            <p class="form__description" data-translate="Connect4 wants to be a fast-paced entertainment site. We'll never ask for your email address to enjoy our content. Sign in or create an account with just your username and password.">
+                Connect4 wants to be a fast-paced entertainment site. We'll never
+                ask for your email address to enjoy our content. Sign in or create
+                an account with just your username and password.
+            </p>
+        </div>
 
         <div class="fieldset">
             <div class="field">
@@ -46,11 +51,16 @@ require_once BASE . 'resources/components/header.php';
 <form id="form-signup" class="form form--hidden form--blue" action="/signup" method="post" novalidate>
     <div class="form__content">
         <h2 class="form__title" data-translate="Sign up">Sign up</h2>
-        <!-- <p class="form__description" data-translate="Connect4 wants to be a fast-paced entertainment site. We'll never ask for your email address to enjoy our content. Sign in or create an account with just your username and password.">
-            Connect4 wants to be a fast-paced entertainment site. We'll never
-            ask for your email address to enjoy our content. Sign in or create
-            an account with just your username and password.
-        </p> -->
+        <div class="form__tooltip">
+            <svg class="icon icon--xs" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16">
+                <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16m.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2"/>
+            </svg>
+            <p class="form__description" data-translate="Connect4 wants to be a fast-paced entertainment site. We'll never ask for your email address to enjoy our content. Sign in or create an account with just your username and password.">
+                Connect4 wants to be a fast-paced entertainment site. We'll never
+                ask for your email address to enjoy our content. Sign in or create
+                an account with just your username and password.
+            </p>
+        </div>
 
         <div class="fieldset">
             <div class="field">
