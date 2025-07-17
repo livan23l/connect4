c55bfb82047a51453374001ab0bcd0690d1a6ceb
Iván Alfredo López Barrera <ivan23.barrera@gmail.com>
2025-07-17 17:22:30 -0600

    Refactor: Refactoring the header, now all the custom headers require the original header because the alerts

--- resources/components/header.php
+++ resources/components/header.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en" data-lang="<?= $_SESSION['language'] ?? '' ?>">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -11,18 +12,27 @@
     <meta name="description" content="This is classic connect four game made with HTML, CSS, and JS, available in Spanish and English. Play alone or with friends and unlock achievements on the site.">
     <meta name="keywords" content="connect 4, connect four, game, online game, connect 4 online, connect 4 offline, connect four online, connect four offline">
     <!-- Include aditional css files -->
-    <?php if(isset($styles)): ?>
-        <?php foreach($styles as $style): ?>
+    <?php if (isset($styles)): ?>
+        <?php foreach ($styles as $style): ?>
             <link rel="stylesheet" href="css/<?= $style ?>.css">
         <?php endforeach; ?>
     <?php endif; ?>
     <!-- Include aditional js files -->
-    <?php if(isset($scripts)): ?>
-        <?php foreach($scripts as $script): ?>
+    <?php if (isset($scripts)): ?>
+        <?php foreach ($scripts as $script): ?>
             <script defer src="js/<?= $script ?>.js"></script>
         <?php endforeach; ?>
     <?php endif; ?>
 </head>
 
-<body>
-    <main <?= isset($main_classes) ? 'class="' . $main_classes . '"' : '' ?>>
+<body class="page">
+    <?php if (isset($_SESSION['alert'])): ?>
+        <div class="server-alert <?= $_SESSION['alert']['type'] ?>">
+            <p class="server-alert__message"><?= $_SESSION['alert']['message'] ?></p>
+        </div>
+        <?php unset($_SESSION['alert']); ?>
+    <?php endif; ?>
+
+    <?php if (!isset($without_main)): ?>
+        <main <?= isset($main_classes) ? 'class="' . $main_classes . '"' : '' ?>>
+    <?php endif; ?>
--- resources/components/header_game.php
+++ resources/components/header_game.php
@@ -1,31 +1,7 @@
-<!DOCTYPE html>
-<html lang="en" data-lang="<?= $_SESSION['language'] ?? '' ?>">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title data-translate="<?= $title ?? "Connect4" ?>"><?= $title ?? "Connect4" ?></title>
-    <base href="<?= HTML_BASE ?>">
-    <link rel="stylesheet" href="css/app.css">
-    <link rel="stylesheet" href="css/game.css">
-    <script type="module" src="js/app.js"></script>
-    <script defer src="js/game.js"></script>
-    <link rel="shortcut icon" href="img/favicon.webp">
-    <meta name="description" content="This is classic connect four game made with HTML, CSS, and JS, available in Spanish and English. Play alone or with friends and unlock achievements on the site.">
-    <meta name="keywords" content="connect 4, connect four, game, online game, connect 4 online, connect 4 offline, connect four online, connect four offline">
-    <!-- Include aditional css files -->
-    <?php if (isset($styles)): ?>
-        <?php foreach ($styles as $style): ?>
-            <link rel="stylesheet" href="css/<?= $style ?>.css">
-        <?php endforeach; ?>
-    <?php endif; ?>
-    <!-- Include aditional js files -->
-    <?php if (isset($scripts)): ?>
-        <?php foreach ($scripts as $script): ?>
-            <script defer src="js/<?= $script ?>.js"></script>
-        <?php endforeach; ?>
-    <?php endif; ?>
-</head>
-
-<body>
-    <main>
+<?php
+$styles = $styles ?? [];
+$scripts = $scripts ?? [];
+array_unshift($styles, 'game');
+array_unshift($scripts, 'game');
+require_once BASE . 'resources/components/header.php';
+?>
