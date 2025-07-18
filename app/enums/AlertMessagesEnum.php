34d00ba37a052bc2c7bfaf59db5a6c1f00dfaee6
Iván Alfredo López Barrera <ivan23.barrera@gmail.com>
2025-07-17 18:45:43 -0600

    Feat: Update the possible alert messages

--- app/enums/AlertMessagesEnum.php
+++ app/enums/AlertMessagesEnum.php
@@ -3,11 +3,17 @@
 enum AlertMessagesEnum: int
 {
     case PASSWORD_UPDATED = 1;
+    case SESSION_CLOSED = 2;
+    case ACCOUNT_DELETED = 3;
+    case UNKNOWN_ERRROR = 4;
 
     public function message()
     {
         return match ($this) {
             self::PASSWORD_UPDATED => "Password updated successfully",
+            self::SESSION_CLOSED => "The session was successfully closed",
+            self::ACCOUNT_DELETED => "The account was successfully deleted",
+            self::UNKNOWN_ERRROR => "There was an unknown error",
         };
     }
 }
