b5f0a306a729b495fc6b28d81ec429ec364d1af6
Iván Alfredo López Barrera <ivan23.barrera@gmail.com>
2025-07-29 02:05:15 -0600

    Chore: Changing the order

--- config/database/seeders/seeder_2025_07_28_02_banners.php
+++ config/database/seeders/seeder_2025_07_28_02_banners.php
@@ -6,9 +6,9 @@ return new class {
         ("city-background", 300),
         ("digital-background", 300),
         ("battle-ships-background", 300),
+        ("arcade-background", 300),
         ("blue-disc-background", 300),
         ("red-disc-background", 300),
-        ("arcade-background", 300),
         ("explosion-background", 300),
         ("cartoon-background", 300);';
         return $sql;
